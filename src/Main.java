09fc19c885368a33f1e7bbfe3889fc4853d724b4
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-05-19 20:46:21 +0200

    User can restart game

--- src/Main.java
+++ src/Main.java
@@ -1,111 +1,117 @@
-import jdk.internal.util.xml.impl.Input;
-
-import javax.swing.*;
-import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     private static final String NAME = "Yuanheng Li";
-    private static final Date DATE = new Date();
+    private static final String DATE = "Sun May 19 17:53:21 CEST 2019";
+    //To record total sum
     private static int sum = 0;
+    //To record how many times user get 6 on a dice
     private static int bonusDiceRound = 0;
 
     public static void main(String[] args) {
+
+        System.out.println();
         printUserInfo();
-        //int diceAmount = getValidUserInput();
-        //System.out.println("Dice amount is: "+diceAmount);
-        //castDice(diceAmount);
-        sum += castBonusDice();
-        System.out.println("Outer sum is: "+sum);
+        play();
     }
 
+    private static void play(){
+        int castAmount = getValidUserInput();
+        System.out.println("Antalet du har vålt är: "+castAmount);
+        System.out.println();
+        castDice(castAmount);
+        //sum += castBonusDice();
+        System.out.println();
+        System.out.println("Summan totalt är: "+sum);
+        restart();
+    }
     private static void printUserInfo(){
         System.out.println(NAME);
         System.out.println(DATE);
         System.out.println();
     }
 
+
+    //To get valid cast amount
     private static int getValidUserInput(){
         while(true){
             try {
                 int userInput = -1;
-                System.out.println("Input how many dice you would like to play(1-5)");
+                System.out.println("Ange hur många tärningar du vill ha:(1-5)");
                 Scanner sc = new Scanner(System.in);
                 if(sc.hasNext()) {
                     userInput = sc.nextInt();
                     if (userInput < 1 || userInput > 5) {
+                        //Handle an integer not within [1,5]
                         throw new SmallerThanZeroOrBiggerThanFiveException();
                     }
                 }
                 return userInput;
             }catch (InputMismatchException mismatchException){
-                System.out.println("Only number is acceptable");
+                System.out.println("Bara ett heltal som som mellan 1 och 5 är giltigt");
             }
             catch (SmallerThanZeroOrBiggerThanFiveException rangeException){
-                System.out.println("Choose a number between 1 and 5");
+                System.out.println("Ange ett heltal som är mellan 1 och 5.");
             }
-
         }
     }
 
-    private static void castDice(int diceAmount){
-        StringBuilder str = new StringBuilder("Sum =");
-
-        for(int diceIndex = 1; diceIndex <= diceAmount; diceIndex++){
+    //To cast dice one by one
+    private static void castDice(int castAmount){
+        for(int diceIndex = 1; diceIndex <= castAmount; diceIndex++){
             int randomDice = (int) Math.floor(Math.random()*6 +1);
-            sum += randomDice;
-            System.out.println("The "+diceIndex +" dice gets "+randomDice+" ,and current sum = "+sum);
-            str.append(" "+randomDice);
-            if(diceIndex<diceAmount)
-                str.append(" +");
+            //When get 6 on a dice
+            if(randomDice == 6){
+               System.out.println("Bonus!Du har fått en sex på din "+ diceIndex + " kast!");
+               sum += castBonusDice();
+            }else
+                sum += randomDice;
+            System.out.println("Den "+diceIndex +" Kasten av alla dina "+castAmount+" tärningar är "+randomDice+" ," +
+                    "hittills har du totalt fått "+bonusDiceRound+" bonus, nävarande summan = "+sum);
         }
-        str.append("= "+sum);
-        System.out.println(str);
     }
-
+    //Use recursive to handle bonus as to cast two more dices
     private static int castBonusDice(){
         bonusDiceRound++;
+        //local bonus field to record which bonus recursive in on
         int localBonusRound = bonusDiceRound;
+        //local sum to record sum of each bonus recursive
         int sum = 0;
-        System.out.println("Bonus!you get a six!This is the "+ localBonusRound+" bonus!");
-        System.out.println("Current sum is: "+sum);
+        System.out.println("Den här är den "+ localBonusRound+" bonus!");
         System.out.println();
-        //System.out.println("Current sum is: "+sum);
         for(int bonusDiceIndex = 1; bonusDiceIndex <= 2; bonusDiceIndex++){
 
             int randomDice = (int) Math.floor(Math.random()*6 +1);
 
             if(randomDice == 6){
-                System.out.println("The "+bonusDiceIndex +" dice on "+localBonusRound+" bonus is "+randomDice);
+                System.out.println("Den "+bonusDiceIndex +" kasten på "+localBonusRound+" bonus är "+randomDice);
                 sum += castBonusDice();
-                //System.out.println("Sum after "+localBonusRound+" bonus, and "+bonusDiceIndex+" dice is: "+sum);
-                System.out.println("Current sum is: "+sum);
+                System.out.println("Närvarande summan inom "+localBonusRound+" bonus är: "+sum);
                 System.out.println();
             }else {
                 sum += randomDice;
-                System.out.println("The "+bonusDiceIndex +" dice on "+localBonusRound+" bonus is "+randomDice);
-                //System.out.println("Sum after "+bonusDiceIndex +" dice on "+localBonusRound+" bonus is "+sum);
-                System.out.println("Current sum is: "+sum);
+                System.out.println("Den "+bonusDiceIndex +" kasten på "+localBonusRound+" bonus är "+randomDice);
+                System.out.println("Närvarande summan inom "+localBonusRound+" bonus är: "+sum);
                 System.out.println();
             }
 
         }
-        //System.out.println();
-       // System.out.println("Sum for "+bonusDiceRound+" bonus is"+ sum);
-       // System.out.println();
         return sum;
     }
-   /*
-    private int cast(int diceNumber) {
-        for (int i = 1; i <= diceNumber; i++) {
-            System.out.println("Recursive on " + i + " dice");
-            int randomDice = (int) Math.floor(Math.random()*6 +1);
 
-            if (randomDice = 6) {}
+    private static void restart(){
+        System.out.println();
+        System.out.println("Vill du starta programmet om?(Y/N)");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        if(userInput.toUpperCase().equals("Y")){
+            sum = 0;
+            bonusDiceRound =0;
+            play();
+        }else {
+            System.exit(0);
         }
     }
-    */
-
 }
